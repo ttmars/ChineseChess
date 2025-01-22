9d1916e01931b0bd7d26d8f2b95c6d3ec35d28fb
Jin <2292752593@qq.com>
2025-01-22 17:00:16 +0800

    画棋盘

--- chess/chess.go
+++ chess/chess.go
@@ -52,6 +52,27 @@ func NewChess() *Chess {
 	// 棋盘面板
 	ChessBoard := canvas.NewRectangle(Yellow)
 	ChessBoard.Resize(fyne.NewSize(700, 600))
+	LineContainer := chess.DrawLine()
+	ChessContainer := container.NewWithoutLayout(
+		ChessBoard, LineContainer,
+	)
+
+	ChessContainer.Resize(fyne.NewSize(700, 600))
+	ChessContainer.Move(fyne.NewPos(300, 0))
+
+	MainWindow.SetContent(container.NewWithoutLayout(ChatContainer, ChessContainer))
+
+	chess.App = app
+	chess.MainWindow = MainWindow
+	chess.ChatContainer = ChessContainer
+	chess.ChatBoard = ChatBoard
+	chess.ChessContainer = ChessContainer
+	chess.ChessBoard = ChessBoard
+	return chess
+}
+
+// DrawLine 画棋盘
+func (chess *Chess) DrawLine() *fyne.Container {
 	// 水平线
 	hor0 := NewLine(chess.Border, chess.Border+chess.Cell*0, chess.Border+chess.Cell*8, chess.Border+chess.Cell*0, chess.LineStrokeWidth*2, color.Black)
 	hor1 := NewLine(chess.Border, chess.Border+chess.Cell*1, chess.Border+chess.Cell*8, chess.Border+chess.Cell*1, chess.LineStrokeWidth, color.Black)
@@ -84,34 +105,22 @@ func NewChess() *Chess {
 	ver66 := NewLine(chess.Border+chess.Cell*6, chess.Border+chess.Cell*5, chess.Border+chess.Cell*6, chess.Border+chess.Cell*9, chess.LineStrokeWidth, color.Black)
 	ver77 := NewLine(chess.Border+chess.Cell*7, chess.Border+chess.Cell*5, chess.Border+chess.Cell*7, chess.Border+chess.Cell*9, chess.LineStrokeWidth, color.Black)
 
+	// 九宫线
 	slash1 := NewLine(chess.Border+chess.Cell*3, chess.Border, chess.Border+chess.Cell*5, chess.Border+chess.Cell*2, chess.LineStrokeWidth*0.5, color.Black)
 	slash2 := NewLine(chess.Border+chess.Cell*5, chess.Border, chess.Border+chess.Cell*3, chess.Border+chess.Cell*2, chess.LineStrokeWidth*0.5, color.Black)
 	slash3 := NewLine(chess.Border+chess.Cell*3, chess.Border+chess.Cell*9, chess.Border+chess.Cell*5, chess.Border+chess.Cell*7, chess.LineStrokeWidth*0.5, color.Black)
 	slash4 := NewLine(chess.Border+chess.Cell*3, chess.Border+chess.Cell*7, chess.Border+chess.Cell*5, chess.Border+chess.Cell*9, chess.LineStrokeWidth*0.5, color.Black)
 
-	ChessContainer := container.NewWithoutLayout(
-		ChessBoard,
+	return container.NewWithoutLayout(
 		hor0, hor1, hor2, hor3, hor4, hor5, hor6, hor7, hor8, hor9,
 		ver0, ver8,
 		ver1, ver2, ver3, ver4, ver5, ver6, ver7,
 		ver11, ver22, ver33, ver44, ver55, ver66, ver77,
 		slash1, slash2, slash3, slash4,
 	)
-
-	ChessContainer.Resize(fyne.NewSize(700, 600))
-	ChessContainer.Move(fyne.NewPos(300, 0))
-
-	MainWindow.SetContent(container.NewWithoutLayout(ChatContainer, ChessContainer))
-
-	chess.App = app
-	chess.MainWindow = MainWindow
-	chess.ChatContainer = ChessContainer
-	chess.ChatBoard = ChatBoard
-	chess.ChessContainer = ChessContainer
-	chess.ChessBoard = ChessBoard
-	return chess
 }
 
+// NewLine 画一条直线
 func NewLine(x1, y1, x2, y2, StrokeWidth float32, color color.Color) *canvas.Line {
 	line := &canvas.Line{
 		Position1:   fyne.Position{X: x1, Y: y1},
